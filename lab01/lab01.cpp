ead6efde30d73e1900105331a1d86e36e8e84603
Billings <billinja@ucdencsecnode02.ucdenver.pvt>
2017-08-31 12:30:04 -0600

    Initial commit with lab01

---
+++ lab01/lab01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 2, y = 3, result;
+	cout << "My name is Jake Billings\n";
+	cout << "My CSCI 1411 instructor is Hawkar Oagaz" << endl;
+	result = x + y;
+	cout << "x + y = " << result << endl;
+}
