b0998a60d2cd52f69a92b907d20a4b951137f6cb
Jake Billings <jake@jakebillings.com>
2017-09-08 16:37:31 -0600

    Update billingsjLab03_plan.cpp

--- lab03/billingsjLab03_plan.cpp
+++ lab03/billingsjLab03_plan.cpp
@@ -14,19 +14,19 @@
                     //If player 1 selected r and player 2 selected s, player 1 wins
                     //If player 1 selected r and player 2 selected p, player 2 wins
                     //If there is not a tie, player 2 did not select r. If player 2 also didn't select p or s, it's not a valid selection.
-                    break; //Unrecognized input for player2
+                        //Unrecognized input for player2
                 //Check if player 1 selected p
                     //If player 1 selected r and player 2 selected r, player 1 wins
                     //If player 1 selected s and player 2 selected p, player 2 wins
                     //If there is not a tie, player 2 did not select p. If player 2 also didn't select r or s, it's not a valid selection.
-                    break; //Unrecognized input for player2
+                        //Unrecognized input for player2
                 //Check if player 1 selected s
                     //If player 1 selected s and player 2 selected p, player 1 wins
                     //If player 1 selected s and player 2 selected r, player 2 wins
                     //If there is not a tie, player 2 did not select s. If player 2 also didn't select r or p, it's not a valid selection.
-                    break; //Unrecognized input for player2 
+                        //Unrecognized input for player2 
                 //If player 1 didn't select r, p, or s, player 1's selection is invalid.
-                    state = INVALID_PLAYER_1; //Unrecognized input for player1
+                    //Unrecognized input for player1
         //Based on the result state, print the appropriate message.
         //Ask the user if he/she would like to play again.       
         //Store the value.
