631c350e8d4857c6535e6ff5d0a76befea4fdbfa
jakebillings <jake@jakebillings.com>
2017-10-02 17:22:32 -0600

    Complete all sections of lab06

--- lab06/lab06/Functions.cpp
+++ lab06/lab06/Functions.cpp
@@ -23,6 +23,7 @@ void handleUnknownInput() {
     cout << "Sorry. I don't know how to handle that option. Please pick from the menu." << endl;
 }
 
+
 /**
  * Reads responses from file into memory
  *
@@ -31,15 +32,21 @@ void handleUnknownInput() {
  * @param maxResponses The maximum number of responses that can fit in the array
  */
 void readResponsesFromFile(Response* responses, unsigned int* responseCount, const unsigned int maxResponses) {
+    //Find out what file to read from
+    string filePath;
+    cout << "What file would you like to read from? (The default responses are in \"responses.bin\".)" << endl;
+    cin.ignore(); //Ignore since we're using getline()
+    getline(cin, filePath);
+
     //Initialize an input stream pointed at the input file
     ifstream input;
-    input.open("responses.bin");
+    input.open(filePath);
 
     //Initialize a counter to track how many responses we read
     unsigned int count = 0;
 
     //Read the responses until we reach the end of the file or fill up the allocated memory.
-    while (input.peek()!=EOF && (*responseCount < maxResponses)) {
+    while (input.peek() != EOF && (*responseCount < maxResponses)) {
         //Initialize a local copy of the response
         Response r;
 
@@ -55,6 +62,9 @@ void readResponsesFromFile(Response* responses, unsigned int* responseCount, con
         *responseCount = *responseCount+1;
     }
 
+    //Close the input stream
+    input.close();
+
     //If the array is full, warn the user that it's full and it's possible not all responses loaded.
     if (*responseCount >= maxResponses) {
         cout << "Warning: the response array is full. There is no guarantee all responses were loaded." << endl;
@@ -63,6 +73,7 @@ void readResponsesFromFile(Response* responses, unsigned int* responseCount, con
     //Print a message stating we loaded the responses
     cout << endl << "Loaded "<< count << " responses. There are now " << *responseCount << " total responses." << endl;
 }
+
 
 /**
  * Executes one round of magic eight ball
@@ -84,6 +95,7 @@ void playGame(Response* responses, unsigned int* responseCount, const unsigned i
     //Print the response
     cout << responses[responseIndex].text << endl;
 }
+
 
 /**
  * Adds one response for the magic eight ball
@@ -113,6 +125,111 @@ void addResponse(Response* responses, unsigned int* responseCount, const unsigne
     *responseCount = *responseCount + 1;
 }
 
+
+/**
+ * Prints all responses in the responses array
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void printResponses(Response* responses, unsigned int* responseCount, const unsigned int maxResponses) {
+    //Iterate through each response index (0, responseCount)
+    for (unsigned int i = 0; i < *responseCount; i++) {
+        //Print the response
+        cout << "Response " << i << ": " << responses[i].text << " (" << responses[i].type << ")" << endl;
+    }
+}
+
+
+/**
+ * Prints all responses in the responses array alphabetically by alphabetizing the array by text in memory then printing it
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void printResponsesAlphabetically(Response* responses, unsigned int* responseCount, const unsigned int maxResponses) {
+    //Sort the responses in memory using bubble sort by text
+    //Perform a double iteration over the array
+    for (unsigned long i = 0; i < *responseCount; i++) {
+        for (unsigned long j = 0; j+1 < *responseCount; j++) {
+            //Cache the response at index j
+            Response r = responses[j];
+
+            //Compare the alphabetical index of responses[j] and responses[j+1] using the > operator
+            if (r.text > responses[j+1].text) {
+
+                //Swap the responses if they are in the wrong order
+                responses[j] = responses[j+1];
+                responses[j+1] = r;
+            }
+        }
+    }
+
+    //Print the responses in the order they're in in memory, which is now alphabetical
+    printResponses(responses, responseCount, maxResponses);
+}
+
+/**
+ * Prints all responses in the responses array alphabetically by alphabetizing the array by type in memory then printing it
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void printResponsesByType(Response* responses, unsigned int* responseCount, const unsigned int maxResponses) {
+    //Sort the responses in memory using bubble sort by type
+    //Perform a double iteration over the array
+    for (unsigned long i = 0; i < *responseCount; i++) {
+        for (unsigned long j = 0; j+1 < *responseCount; j++) {
+            //Cache the response at index j
+            Response r = responses[j];
+
+            //Compare the alphabetical index of responses[j] and responses[j+1] using the > operator
+            if (r.type > responses[j+1].type) {
+
+                //Swap the responses if they are in the wrong order
+                responses[j] = responses[j+1];
+                responses[j+1] = r;
+            }
+        }
+    }
+
+    //Print the responses in the order they're in in memory, which is now alphabetical
+    printResponses(responses, responseCount, maxResponses);
+}
+
+
+/**
+ * Writes all responses in the responses array to a file
+ *
+ * @param responses The array of responses to read out of
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void writeResponsesToFile(Response* responses, unsigned int* responseCount, const unsigned int maxResponses) {
+    //Find out what file to read from
+    string filePath;
+    cout << "What file would you like to write to? (I recommend ending this file in \".bin\" for consistency.)" << endl;
+    cin.ignore(); //Ignore since we're using getline()
+    getline(cin, filePath);
+
+    //Open an output stream to a file
+    ofstream output;
+    output.open(filePath);
+
+    //For each response, write it to the output stream in the same format as the input file
+    for (unsigned long i = 0; i < *responseCount; i++) {
+        output << responses[i].text << endl;
+        output << responses[i].type << endl;
+    }
+
+    //Close the output stream
+    output.close();
+}
+
+
 /**
  * Executes one iteration of the menu loop.
  * 
@@ -129,12 +246,12 @@ bool menuIteration(Response* responses, unsigned int* responseCount, const unsig
 	cout << "f.\tWrite responses to a file" << endl;
 	cout << "g.\tExit" << endl;
 
-	//Allocate a space in memory for the user's answer
+	//Allocate a space in memory for the user's answer and read it using cin
 	char inputSelectionRaw;
 	cout << "Which option would you like to select?" << endl;
 	cin >> inputSelectionRaw;
 
-	//Convert the selection to lowercase
+	//Convert the selection to lowercase for easier switch cases
 	char inputSelection = tolower(inputSelectionRaw);
 
 	//Run the correct function based on the selection
@@ -148,11 +265,22 @@ bool menuIteration(Response* responses, unsigned int* responseCount, const unsig
 	    case 'c':
             addResponse(responses, responseCount, maxResponses);
             break;
+	    case 'd':
+            printResponsesAlphabetically(responses, responseCount, maxResponses);
+            break;
+	    case 'e':
+            printResponsesByType(responses, responseCount, maxResponses);
+            break;
+	    case 'f':
+            writeResponsesToFile(responses, responseCount, maxResponses);
+            break;
         case 'g':
             return false;
         default:
             handleUnknownInput();
 	}
 
+    //Return true because we want to complete the loop again
+    // We would have returned false if we didn't.
     return true;
 }
--- lab06/lab06/Functions.h
+++ lab06/lab06/Functions.h
@@ -25,6 +25,82 @@ struct Response {
     string type;
 };
 
+
+/**
+ * Prints an error message for when a user enters an invalid selection
+ */
+void handleUnknownInput();
+
+
+/**
+ * Reads responses from file into memory
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void readResponsesFromFile(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+
+/**
+ * Executes one round of magic eight ball
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void playGame(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+
+/**
+ * Adds one response for the magic eight ball
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void addResponse(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+
+/**
+ * Prints all responses in the responses array
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void printResponses(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+
+/**
+ * Prints all responses in the responses array alphabetically by alphabetizing the array by text in memory then printing it
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void printResponsesAlphabetically(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+/**
+ * Prints all responses in the responses array alphabetically by alphabetizing the array by type in memory then printing it
+ *
+ * @param responses The array of responses to read into
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void printResponsesByType(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+
+/**
+ * Writes all responses in the responses array to a file
+ *
+ * @param responses The array of responses to read out of
+ * @param responseCount The number of responses already in that array
+ * @param maxResponses The maximum number of responses that can fit in the array
+ */
+void writeResponsesToFile(Response* responses, unsigned int* responseCount, const unsigned int maxResponses);
+
+
 /**
  * Executes one iteration of the menu loop.
  *
