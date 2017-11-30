c003fd6b8c6acfe66e1d0976b957c2c7a02e5bb0
Jake Billings <jake.billings@ucdenver.edu>
2017-11-30 13:05:39 -0700

    Move all of the bash tools I used throughout the class to one folder on the repository's root.

--- lab13/plan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find  -type f -name "*.cpp" -print0 |
-while IFS= read -r -d '' pathname; do
-    echo "Filtering ${pathname} to ${pathname}_plan.cpp..."
-    cat "${pathname}" | grep -E "(//|/\*| \*)" > "${pathname%.cpp}_plan.cpp"
-    echo "Done planning ${pathname}"
-done
